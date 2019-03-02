eaf229f4ba2632a1f2a9f2b47cbc61e5f20a203e
lphanvilay <linda.phanvilay@mavs.uta.edu>
2019-03-02 15:20:06 -0600

    Added Structures and Weights Code

---
+++ StructuresCode.m
@@ -0,0 +1,26 @@
+%Areas
+    %Wings
+S = ;
+b = ;
+chord = S/b;
+AR = b*b/S;
+taperRatio = ;
+    %Fuselage
+WettedArea =
+DryArea = 
+
+%Tensions
+Force =
+Area = ;
+fTen = ForceT/AreaT
+%Bending
+M = ;% moment
+c = ; %distance to neutral axis
+I = ;% 
+%Compression
+Pc = ; 
+MoS = StressAll/StressAppl-1;
+%Six components of structural loading
+Px = ;
+Py = ;
+Pz = ;
---
+++ WEIGHTS.m
@@ -0,0 +1,9 @@
+%Weight Estimation Program
+%Utilizing WAAT
+Ncrew = %Number of crew members
+
+Neng = Nre + Nrj + Ntrj; %calculate number of engines
+Ttot = Nre*Tre + Nrj*Trj + Ntrj*Ttrj;
+Wpayload = %Payload Weight
+
+%Iterative process to get gross weight
